c7dd8424dd515adf64abedb5ad7badd153f2f39d
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-09-14 16:04:48 +0800

    bug修复

--- src/app/api/visaTable/route.ts
+++ src/app/api/visaTable/route.ts
@@ -14,22 +14,6 @@ export const GET = async () => {
   }
 };
 
-// export const POST = async (request: Request) => {
-//     try {
-//         connectToDb();
-//         const body:unknown = await request.json()
-//         const params = newFormSchema.safeParse(body)
-//         if(!params.success){
-//             throw new Error(params.error.issues[0].message)
-//         }
-//         let res = await Record.create(params.data)
-
-//         return NextResponse.json({})
-//     }catch(error){
-//         throw new Error((error as Error).message);
-//     }
-// }
-
 export const POST = async (request: Request) => {
   try {
     await connectToDb();
--- src/app/dashboard/table/page.tsx
+++ src/app/dashboard/table/page.tsx
@@ -5,7 +5,7 @@ import { DataTable } from "./data-table";
 import { generateFakeRecords } from "@/lib/fakeData";
 async function getData(): Promise<Record[]> {
   // Fetch data from your API here.
-  let res = await fetch("http://localhost:3000/api/upload", {
+  let res = await fetch("http://localhost:3000/api/visaTable", {
     cache: "no-store",
   });
   if (!res.ok) {
