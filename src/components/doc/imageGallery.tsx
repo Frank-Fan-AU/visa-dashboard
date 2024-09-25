3deba46ac4ab831e290cfee0a36fe74809364b23
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-09-25 14:18:27 +0800

    更改问本地存储图片

--- src/components/doc/imageGallery.tsx
+++ src/components/doc/imageGallery.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
 interface ImageGalleryProps {
+    path: string;
     count: number;
   }
 
-  const ImageGallery: React.FC<ImageGalleryProps> = ({ count }) => {
+  const ImageGallery: React.FC<ImageGalleryProps> = ({path, count }) => {
     const images = Array.from({ length: count }, (_, i) => i + 1);
   
     return (
@@ -12,7 +13,7 @@ interface ImageGalleryProps {
         {images.map((num) => (
           <Image
             key={num}
-            src={`https://zejie-img-storage.s3.amazonaws.com/visadashboard/80form/p${num}.png`}
+            src={`/${path}/p${num}.png`}
             alt={`pic${num}`}
             width={1200}
             height={600}
