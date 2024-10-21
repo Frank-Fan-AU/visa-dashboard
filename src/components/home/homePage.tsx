fc39861dd8fb28d390ec5c4bc218587395c31361
Fanzejie <fanzejiea@gmail.com>
2024-10-21 20:45:46 +0800

    1

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     <div className="flex flex-col lg:flex-row items-center justify-center  px-8 mb-4">
       <div className="w-full lg:w-1/2">
         <HeroHighlight >
-        <h1 className="text-5xl font-bold text-black px-4 pb-4">AU-500-VISA-Dashboard</h1>
+        <h1 className="text-5xl font-bold text-black px-4 pb-4 mt-8">AU-500-VISA-Dashboard</h1>
           <h1 className="text-5xl font-bold text-black px-4 py-2">欢迎👏🏻 </h1>
           <h2 className="text-3xl font-bold text-black px-4 pt-2">
             更容易获取所有要关心的签证信息
