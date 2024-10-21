c6ed184561db7b4ec4c10b07c6a720466f0e9ac7
Fanzejie <fanzejiea@gmail.com>
2024-10-21 20:44:27 +0800

    text fix

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -13,7 +13,7 @@ const HomePage = () => {
         <h1 className="text-5xl font-bold text-black px-4 pb-4">AU-500-VISA-Dashboard</h1>
           <h1 className="text-5xl font-bold text-black px-4 py-2">欢迎👏🏻 </h1>
           <h2 className="text-3xl font-bold text-black px-4 pt-2">
-            旨在帮您更容易得获取所有要关心的签证信息
+            更容易获取所有要关心的签证信息
           </h2>
           <div className="text-black py-10 px-4">
             <Highlight>分享您的签证进度，</Highlight>
