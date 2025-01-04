c66c9380f6d67f65dd0b40be4413a20edb6e777a
FanZejie <61533673+FanZejie@users.noreply.github.com>
2025-01-04 16:24:38 +0800

    添加google-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar/navbar";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,11 +23,27 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-SMCJYMH0HP"
+          ></Script>
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+
+              gtag('config', 'G-SMCJYMH0HP');
+            `}
+          </Script>
+        </head>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
             fontSans.variable
-          )}>
+          )}
+        >
           {/* <Navbar /> */}
           {children}
         </body>
